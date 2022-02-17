ee21ea4da2df35d1a1f8e9343d73d9c679f3c820
yohanankling <93263233+yohanankling@users.noreply.github.com>
2022-02-17 16:58:37 +0200

    added gently exit, some improvement

---
+++ multyclient.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+import sys
+name = input("enter your nickname please:")
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host_name = socket.gethostname()
+ip = socket.gethostbyname(host_name)
+port = 55001
+for i in range(14):
+      try:
+            s.bind(("", port))
+            break
+      except:
+            port = port + 1
+s.connect((ip, 55000))
+print("hello " + name + "!")
+print("here is the commands manual : ")
+print('to send a message to everyone enter - "broadcast,text"')
+print('to send a message to a person enter - "person name,text"')
+print('to see who is online enter - "online"')
+print('to see the available online file list enter - "files"')
+print('to disconnect enter - "exit"')
+
+def send():
+      s.send(name.encode())
+      while True:
+            s.send(input().encode())
+
+t = threading.Thread(target=send)
+t.start()
+
+while True:
+      try:
+            data = s.recv(1024).decode()
+            if data == "exit":
+                  s.close()
+            else:
+                  print(data)
+      except:
+            pass
---
+++ multyserver.py
@@ -0,0 +1,113 @@
+import socket
+import select
+
+server = socket.socket()
+port = 55000
+server.bind(('', port))
+server.listen(15)
+inputs = [server]
+names = {}
+files = ("mygif.gif", "text.txt", "photo.jpg")
+print("ready to serve...")
+
+def welcome(client):
+    name = client.recv(1024)
+    name = name.decode('UTF-8')
+    users = [n.getpeername() for n in inputs if n is not client and n is not server]
+    if len(users) < 1:
+        greetMsg = name + ", you are the first client and no one online yet..so...how you doing?"
+        client.send(greetMsg.encode())
+    else:
+        onlineList(client)
+    client.getsockname()
+    return name
+
+def onlineList(i):
+    msg = "list of who is online:"
+    msg = msg.encode()
+    i.send(msg)
+    data = names.items()
+    for name in data:
+        name = (name[1]+ "\n").encode()
+        i.send(name)
+
+def getClient(name):
+    for key, value in names.items():
+        if name == value:
+            return key
+    return "exep"
+
+def broadcast(msg, non_receptors):
+    for connection in inputs:
+        if connection not in non_receptors:
+            connection.send(msg)
+
+def chatWith (msg, port):
+    for connection in inputs:
+        try:
+            connection.getpeername()[1]
+        except:
+            continue
+        if connection.getpeername()[1] == port:
+            connection.send(msg)
+
+def filesList(i):
+    msg = "list of available files:"
+    msg = msg.encode()
+    i.send(msg)
+    for file in files:
+        file = (file + "\n").encode()
+        i.send(file)
+
+def kick(i):
+    msg = "exit"
+    msg = msg.encode()
+    i.send(msg)
+    id = i.getpeername()[1]
+    del names[id]
+    inputs.remove(i)
+    print(f"client {nickname} has left the chat ")
+    broadcast(f"{nickname} has left the chat".encode(), [server])
+    i.close()
+
+while inputs:
+    readables, _, _ = select.select(inputs, [], [])
+    for i in readables:
+        if i is server:
+            client, address = server.accept()
+            inputs.append(client)
+            name = welcome(client)
+            print("new user detected! " + name + " connected")
+            names[address[1]] = name
+            broadcast(name.encode() + f" entered the chat".encode(), [server, client])
+
+        else:
+            id = i.getpeername()[1]
+            nickname = names[id]
+            try:
+                data = i.recv(1024)
+                data = data.decode('UTF-8')
+                try:
+                    command, message = data.split(',', 1)
+                    data = str(nickname + " >>> " + message).encode()
+                except:
+                    command = data
+                if command == 'broadcast':
+                    broadcast(data, [server, i])
+                elif command == "online":
+                    onlineList(i)
+                elif command == "files":
+                    filesList(i)
+                elif command == "exit":
+                    kick(i)
+                else:
+                    port = getClient(command)
+                    if port == "exep":
+                        msg = "there is no such user name / command ! try again, pay intention to spaces or correct name!"
+                        msg = msg.encode()
+                        i.send(msg)
+                    else:
+                        chatWith(data, port)
+
+            except Exception as e:
+                kick(i)
