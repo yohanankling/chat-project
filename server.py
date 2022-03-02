a30c100cbfadcfe281228e8d0bbd46f5b61560bc
yohanankling <93263233+yohanankling@users.noreply.github.com>
2022-03-03 01:46:35 +0200

    Update server.py

--- server.py
+++ server.py
@@ -4,30 +4,32 @@ import os
 from os import listdir
 from os.path import isfile, join
 
-server = socket.socket()
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+tcp.setblocking(1)
+tcp.setsockopt(socket.SOL_TCP, socket.TCP_QUICKACK, 1)
+tcp.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+
 port = 55000
 buffer = 1024
-# udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(('', port))
-server.listen(15)
-inputs = [server]
+tcp.bind(('', port))
+tcp.listen(15)
+inputs = [tcp]
 names = {}
 print("ready to serve...")
 
 def welcome(client):
     name = client.recv(buffer)
     name = name.decode('UTF-8')
-    users = [n.getpeername() for n in inputs if n is not client and n is not server]
+    users = [n.getpeername() for n in inputs if n is not client and n is not tcp]
     if len(users) < 1:
         greetMsg = name + ", you are the first client so...how you doing?"
         client.send(greetMsg.encode())
     else:
         onlineList(client)
-    client.getsockname()
     return name
 
 def onlineList(i):
-    msg = "list of who is online:"
+    msg = "list of who is online:\n"
     msg = msg.encode()
     i.send(msg)
     data = names.items()
@@ -46,14 +48,18 @@ def broadcast(msg, non_receptors):
         if connection not in non_receptors:
             connection.send(msg)
 
-def chatWith (msg, port):
+def chatWith (port, msg):
     for connection in inputs:
+        if msg.find(">>>") == -1:
+            break
         try:
             connection.getpeername()
         except:
             continue
         if connection.getpeername() == port:
+            msg = msg.encode()
             connection.send(msg)
+            break
 
 def filesList(i):
     path = os.path.abspath("")
@@ -64,32 +70,16 @@ def filesList(i):
     for file in files:
         file = (file + "\n").encode()
         i.send(file)
-#
-# def download(i, file):
-#     path = os.path.abspath("")
-#     files = [file for file in listdir(path) if isfile(join(path, file))]
-#     if file not in files:
-#         msg = "no such a file in the server!"
-#         msg = msg.encode()
-#         i.send(msg)
-#         print("no such a file!")
-#     else:
-#         print(f"sending {file} to {nickname}")
-#         msg = "-sending-"
-#         msg = msg.encode()
-#         i.send(msg)
-#         msg = "file from the server-" + file
-#         msg = msg.encode()
-#         i.send(msg)
-#         size = os.path.getsize(file)
-#         size = str(size).encode()
-#         i.send(size)
-#         bytesToSend = "msgFromClient"
-#         bytesToSend = bytesToSend.encode()
-#         id = i.getpeername()
-#         udpAdd = (id[0], id[1] + 1000)
-#         udp.sendto(bytesToSend, udpAdd)
-#         print(5)
+
+
+def udp(file):
+    msgFromServer = "Hello UDP Client"
+    bytesToSend = str.encode(msgFromServer)
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+    address = (id[0], id[1])
+    print(address)
+    while (True):
+        UDPServerSocket.sendto(bytesToSend, address)
 
 
 
@@ -106,51 +96,12 @@ def download(i, file):
         msg = "-sending-"
         msg = msg.encode()
         i.send(msg)
-        msg = "file from the server-" + file
+        size = os.path.getsize(file)
+        size = str(size)
+        msg = "serverFile-" + file + "," + size
         msg = msg.encode()
         i.send(msg)
-        size = os.path.getsize(file)
-        size = str(size).encode()
-        i.send(size)
-
-        localIP = "127.0.0.1"
-
-        localPort = 20001
-
-        bufferSize = 1024
-
-        msgFromServer = "Hello UDP Client"
-
-        bytesToSend = str.encode(msgFromServer)
-
-        # Create a datagram socket
-
-        UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-
-        # Bind to address and ip
-
-        UDPServerSocket.bind((localIP, localPort))
-
-        print("UDP server up and listening")
-
-        # Listen for incoming datagrams
-
-        while (True):
-            bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-
-            message = bytesAddressPair[0]
-
-            address = bytesAddressPair[1]
-
-            clientMsg = "Message from Client:{}".format(message)
-            clientIP = "Client IP Address:{}".format(address)
-
-            print(clientMsg)
-            print(clientIP)
-
-            # Sending a reply to client
-
-            UDPServerSocket.sendto(bytesToSend, address)
+        udp(file)
 
 
 def kick(i):
@@ -167,9 +118,12 @@ def kick(i):
         del names[id]
         print(f"client {nickname} has left the chat, port {id[1]} free now ")
     except:
-        print(f"client {nickname} has left the chat, port {id[1]} free now ")
+        try:
+            print(f"client {nickname} has left the chat, port {id[1]} free now ")
+        except:
+            pass
     inputs.remove(i)
-    broadcast(f"{nickname} has left the chat".encode(), [server])
+    broadcast(f"{nickname} has left the chat".encode(), [tcp])
     i.close()
 
 def gentlyKick(i, flag):
@@ -177,7 +131,7 @@ def gentlyKick(i, flag):
     try:
         del names[id]
         if flag == 0:
-            broadcast(f"{nickname} has left the chat".encode(), [server])
+            broadcast(f"{nickname} has left the chat".encode(), [tcp])
             print(f"client {nickname} has left the chat, port {id} free now ")
 
     except:
@@ -188,11 +142,11 @@ def gentlyKick(i, flag):
     i.close()
 
 
-while inputs:
+while True:
     readables, _, _ = select.select(inputs, [], [])
     for i in readables:
-        if i is server:
-            client, address = server.accept()
+        if i is tcp:
+            client, address = tcp.accept()
             inputs.append(client)
             name = welcome(client)
             if name in names.values():
@@ -204,8 +158,7 @@ while inputs:
                 continue
             print("new user detected! " + name + " connected")
             names[address] = name
-            broadcast(name.encode() + f" entered the chat".encode(), [server, client])
-
+            broadcast(name.encode() + f" entered the chat".encode(), [tcp, client])
         else:
             try:
                 id = i.getpeername()
@@ -215,28 +168,37 @@ while inputs:
                     data = data.decode('UTF-8')
                     try:
                         command, message = data.split(',', 1)
-                        data = str(nickname + " >>> " + message).encode()
+                        data = str(nickname + " >>> " + message)
                     except:
                         command = data
                     if command == 'broadcast':
-                        broadcast(data, [server, i])
+                        broadcast(data.encode(), [tcp, i])
                     elif command == "online":
                         onlineList(i)
                     elif command == "files":
                         filesList(i)
+
+
+
+                    elif command == "d":
+                        download(i, "text.txt")
+
+
+
+
                     elif command == "download":
                         download(i, message)
                     elif command == "exit":
                         kick(i)
                     else:
                         port = getClient(command)
-                        if port == "exep":
+                        if port == "exep" or data.find(">>>") == -1:
                             msg = "there is no such user name / command ! try again, pay intention to spaces or correct name!"
                             msg = msg.encode()
                             i.send(msg)
                         else:
-                            chatWith(data, port)
-                except Exception as e:
+                            chatWith(port, data)
+                except:
                     try:
                         gentlyKick(i, 0)
                     except:
